30ca39d30a2da4889fbb374ed68003eb948d9bbc
Ulrich Dallmann <ulrich@dallmann-consulting.de>
2021-01-02 22:51:54 +0100

    DB-Changes for OCPP2.0

--- SQLite/OCPP.Core.sqlite.sql
+++ SQLite/OCPP.Core.sqlite.sql
@@ -1,10 +1,12 @@
 BEGIN TRANSACTION;
+DROP TABLE IF EXISTS "ChargePoint";
 CREATE TABLE IF NOT EXISTS "ChargePoint" (
 	"ChargePointId"	TEXT NOT NULL UNIQUE,
 	"Name"	TEXT,
 	"Comment"	TEXT,
 	PRIMARY KEY("ChargePointId")
 );
+DROP TABLE IF EXISTS "ChargeTags";
 CREATE TABLE IF NOT EXISTS "ChargeTags" (
 	"TagId"	TEXT NOT NULL UNIQUE,
 	"TagName"	TEXT,
@@ -13,6 +15,7 @@ CREATE TABLE IF NOT EXISTS "ChargeTags" (
 	"Blocked"	INTEGER,
 	PRIMARY KEY("TagId")
 );
+DROP TABLE IF EXISTS "MessageLog";
 CREATE TABLE IF NOT EXISTS "MessageLog" (
 	"LogId"	INTEGER NOT NULL UNIQUE,
 	"LogTime"	TEXT,
@@ -23,17 +26,19 @@ CREATE TABLE IF NOT EXISTS "MessageLog" (
 	"ErrorCode"	TEXT,
 	PRIMARY KEY("LogId" AUTOINCREMENT)
 );
+DROP TABLE IF EXISTS "Transactions";
 CREATE TABLE IF NOT EXISTS "Transactions" (
 	"TransactionId"	INTEGER NOT NULL UNIQUE,
+	"Uid"	TEXT,
 	"ChargePointId"	TEXT,
 	"ConnectorId"	INTEGER,
 	"StartTagId"	TEXT,
 	"StartTime"	TEXT,
-	"MeterStart"	INTEGER,
+	"MeterStart"	REAL,
 	"StartResult"	TEXT,
 	"StopTagId"	TEXT,
 	"StopTime"	TEXT,
-	"MeterStop"	INTEGER,
+	"MeterStop"	REAL,
 	"StopReason"	TEXT,
 	PRIMARY KEY("TransactionId" AUTOINCREMENT)
 );
