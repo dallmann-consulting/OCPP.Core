fcf88c48f41e8d09a1840394855248a1345ddacd
Ulrich Dallmann <ulrich@dallmann-consulting.de>
2024-05-16 19:10:56 +0200

    Update script for SQL-Server

---
+++ SQL-Server/Update_to_V132.sql
@@ -0,0 +1,16 @@
+USE [OCPP.Core]
+GO
+
+/**** New with V1.3.2 ****/
+
+SET ANSI_PADDING ON
+GO
+
+CREATE NONCLUSTERED INDEX [IX_Transactions_ChargePointId_ConnectorId] ON [dbo].[Transactions]
+(
+	[ChargePointId] ASC,
+	[ConnectorId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+
