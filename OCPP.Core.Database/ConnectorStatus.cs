2fea606d5a80d39e9c601b93a4b957ea12d422c9
Jeremy Mahieu <JM@dynamics.be>
2024-05-24 13:49:45 +0200

    Refactor export, use the closedxml libary, add xlsx export

--- OCPP.Core.Database/ConnectorStatus.cs
+++ OCPP.Core.Database/ConnectorStatus.cs
@@ -18,7 +18,6 @@
  */
 
 using System;
-using System.Collections.Generic;
 
 #nullable disable
 
@@ -28,13 +27,23 @@ namespace OCPP.Core.Database
     {
         public string ChargePointId { get; set; }
         public int ConnectorId { get; set; }
-
         public string ConnectorName { get; set; }
-
         public string LastStatus { get; set; }
         public DateTime? LastStatusTime { get; set; }
-
         public double? LastMeter { get; set; }
         public DateTime? LastMeterTime { get; set; }
+        public virtual ChargePoint ChargePoint { get; set; }
+
+        public override string ToString()
+        {
+            string chargePointName = ChargePoint?.Name ?? ChargePointId;
+            if (!string.IsNullOrEmpty(ConnectorName))
+            {
+                return ConnectorName;
+            }
+
+            return $"{chargePointName}:{ConnectorId}";
+        }
     }
 }
+
