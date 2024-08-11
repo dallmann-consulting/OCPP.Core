11f9670c9aa51c335c3e3b12b63797a9d8f8cf89
Ulrich Dallmann <ulrich@dallmann-consulting.de>
2024-08-11 21:20:10 +0200

    Update SQL-Script to include V1.3.2 change

--- SQL-Server/Create_OCPP.Core.sql
+++ SQL-Server/Create_OCPP.Core.sql
@@ -143,6 +143,12 @@ REFERENCES [dbo].[Transactions] ([TransactionId])
 GO
 ALTER TABLE [dbo].[Transactions] CHECK CONSTRAINT [FK_Transactions_Transactions]
 GO
+CREATE NONCLUSTERED INDEX [IX_Transactions_ChargePointId_ConnectorId] ON [dbo].[Transactions]
+(
+	[ChargePointId] ASC,
+	[ConnectorId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
 USE [master]
 GO
 ALTER DATABASE [OCPP.Core] SET  READ_WRITE 
